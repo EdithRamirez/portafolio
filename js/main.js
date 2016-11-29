d2d75f5c268c73d911a6a27f6610a4bee61cf86c
EdithRamirez <ediths.ramirez07@gmail.com>
2016-11-29 13:26:08 -0600

    V5 Final

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-
-	var height = $(window).height();
-	ajustesIniciales();
-
+	
 	//posicion de seccion skills
-	var posicionSkills = $("#skills").position().top;
+	var posicionSkills = $('#skills').position().top;
+	var height = $(window).height();
+	
+	ajustesIniciales();
 
 	function ajustesIniciales() {
 		$('section#sobreMi').css({'margin-top': height - 0 + 'px'});
@@ -34,7 +34,7 @@ $(document).ready(function() {
 	});
 
 	//efecto de mi nombre
-	$(".intro").flipping_text({
+	$('.intro').flipping_text({
 		tickerTime: 40, // Define la rapidez con la que se escribe, el tiempo es en milisegundos
 		customRandomChar: false, //Cadena de caracteres que se escribe en forma aleatoria: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 		tickerCount: 10, //Controla cuantas veces se sustituyen los caracteres hasta visualizar el texto
@@ -51,7 +51,7 @@ $(document).ready(function() {
 	
 	//cierra el menu al darle un clic al enlace GM+200
 	$('.menu').on('click' , function(){
-		$(".navicon.navicon--active").trigger("click")	
+		$('.navicon.navicon--active').trigger('click');	
 	});
 	
 	//wow para efectos en seccion proyecto
@@ -71,6 +71,3 @@ $(document).ready(function() {
 	);
 	wow.init();
 });
-
-
-
